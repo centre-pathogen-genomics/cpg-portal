9bd8be61e0f470c4b98237e9191bc9cb331bf8b9
Wytamma Wirth <wytamma.wirth@me.com>
2025-01-20 13:11:59 +1100

    :sparkles: reset selected files on reset

--- frontend/src/components/Tools/RunToolForm.tsx
+++ frontend/src/components/Tools/RunToolForm.tsx
@@ -10,7 +10,7 @@ import {
   Input,
 } from "@chakra-ui/react"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { Select, SelectInstance, useChakraSelectProps } from "chakra-react-select"
+import { Select, } from "chakra-react-select"
 import { type SubmitHandler, useForm } from "react-hook-form"
 import {
   CreateRunData,
@@ -20,10 +20,75 @@ import {
 } from "../../client"
 import { createRunMutation, readToolParamsOptions } from "../../client/@tanstack/react-query.gen"
 import useCustomToast from "../../hooks/useCustomToast"
-import React, { useState } from "react"
+import React, { useMemo, useState } from "react"
 import { handleError } from "../../utils"
 import { Options } from "@hey-api/client-axios"
 import FileUpload from "../Files/UploadFileWithProgress"
+
+
+interface EnumParamProps {
+  param: Param
+  setValue: (value: any) => void
+}
+
+const EnumParam = ({ param, setValue }: EnumParamProps) => {
+  return (
+    <Select
+        id={param.name}
+        options={param.options?.map((option) => ({
+          label: option,
+          value: option,
+        }))}
+        placeholder={param.description || "Select an option"}
+        defaultValue={
+          param.default
+            ? { label: param.default, value: param.default }
+            : undefined
+        }
+        isMulti={false}
+        onChange={(selectedOption) => {
+          setValue(
+            selectedOption ? selectedOption.value : "",
+          )
+        }}
+        selectedOptionStyle="check"
+      />
+  )
+}
+
+interface FileParamProps {
+  param: Param
+  files: { label: string, value: string }[]
+  setValue: (value: string) => void
+  selectedOptions: { label: string, value: string }[]
+  setSelectedOptions: (options: { label: string, value: string }[]) => void
+}
+
+const FileParam = ({ param, files, setValue, selectedOptions, setSelectedOptions }: FileParamProps) => {
+  return (
+    <Flex direction={"column"}>
+      <FileUpload
+        onComplete={(file) => {
+          setValue(file.id);
+          setSelectedOptions([{ label: file.name, value: file.id }]);
+        }}
+      />
+      <Select
+        id={param.name}
+        options={files}
+        placeholder={param.description || "Select a file"}
+        isMulti={false}
+        value={selectedOptions}
+        onChange={(selectedOption) => {
+          setValue(selectedOption ? selectedOption.value : "");
+          setSelectedOptions(selectedOption ? [selectedOption] : []);
+        }}
+        selectedOptionStyle="check"
+      />
+    </Flex>
+  );
+};
+
 
 interface RunToolFormProps {
   toolId: string
@@ -39,7 +104,7 @@ const RunToolForm = ({ toolId, onSuccess }: RunToolFormProps) => {
     ...readToolParamsOptions({path: {tool_id: toolId}}),  
   })
 
-  const { data: files, isLoading: filesLoading, refetch } = useQuery({
+  const { data, isLoading: filesLoading } = useQuery({
     queryKey: ["files"],
     queryFn: () =>
       FilesService.readFiles().then(({data: files}) =>
@@ -50,6 +115,8 @@ const RunToolForm = ({ toolId, onSuccess }: RunToolFormProps) => {
           }))
       ),
   })
+  
+  let files = data ?? []
 
   const defaultValues = params?.reduce((acc, param) => {
     acc[param.name] = param.default
@@ -95,21 +162,30 @@ const RunToolForm = ({ toolId, onSuccess }: RunToolFormProps) => {
     setIsLoading(true);
     mutation.mutate({body: formData} as Options<CreateRunData>)
   }
-  const [selectedOptions, setSelectedOptions] = useState<{label: string, value: string}[]>([])
+  const normalizedParams = useMemo(() => params || [], [params]);
+ 
+  const [fileStates, setFileStates] = useState<Record<string, { label: string; value: string }[]>>({});
+
+  const handleFileSelection = (paramName: string, selectedOptions: { label: string; value: string }[]) => {
+    setFileStates((prev) => ({ ...prev, [paramName]: selectedOptions }));
+  };
+
 
   if (paramsLoading || filesLoading) return <div>Loading...</div>
+
+
   
   return (
     <Box as="form" onSubmit={handleSubmit(onSubmit)} w="100%">
       <Box>
-        {params?.map((param: Param) => (
+        {normalizedParams?.map((param: Param) => (
           <FormControl
             pb={4}
             key={param.id}
             isRequired={param.required}
             isInvalid={errors[param.name] !== undefined}
           >
-            <FormLabel htmlFor={param.name}>{param.name}</FormLabel>
+            <FormLabel htmlFor={param.name}>{param.name} ({param.description})</FormLabel>
             {param.param_type === "str" && (
               <Input
                 id={param.name}
@@ -146,27 +222,7 @@ const RunToolForm = ({ toolId, onSuccess }: RunToolFormProps) => {
               />
             )}
             {param.param_type === "enum" && (
-              <Select
-                id={param.name}
-                options={param.options?.map((option) => ({
-                  label: option,
-                  value: option,
-                }))}
-                placeholder={param.description || "Select an option"}
-                defaultValue={
-                  param.default
-                    ? { label: param.default, value: param.default }
-                    : undefined
-                }
-                isMulti={false}
-                onChange={(selectedOption) => {
-                  setValue(
-                    param.name,
-                    selectedOption ? selectedOption.value : "",
-                  )
-                }}
-                selectedOptionStyle="check"
-              />
+              <EnumParam param={param} setValue={(value) => setValue(param.name, value)} />
             )}
             {param.param_type === "bool" && (
               <Checkbox
@@ -180,30 +236,13 @@ const RunToolForm = ({ toolId, onSuccess }: RunToolFormProps) => {
               </Checkbox>
             )}
             {param.param_type === "file" && (
-             <Flex
-              direction={"column"}
-            >
-                <FileUpload onComplete={(file) => {
-                    setValue(param.name, file.id);
-                    setSelectedOptions([{label: file.name, value: file.id}]) 
-                  }
-                } />
-                <Select
-                  id={param.name}
-                  options={files}
-                  placeholder={param.description || "Select a file"}
-                  isMulti={false}
-                  value={selectedOptions}
-                  onChange={(selectedOption) => {
-                    setValue(
-                      param.name,
-                      selectedOption ? selectedOption.value : "",
-                    );
-                    setSelectedOptions(selectedOption ? [selectedOption] : [])
-                  }}
-                  selectedOptionStyle="check"
-                />
-              </Flex>
+              <FileParam
+                param={param}
+                files={files}
+                setValue={(value) => setValue(param.name, value)}
+                selectedOptions={fileStates[param.name] || []}
+                setSelectedOptions={(selectedOptions) => handleFileSelection(param.name, selectedOptions)}
+              />
             )}
             {errors[param.name] && (
               <FormErrorMessage>
@@ -217,7 +256,7 @@ const RunToolForm = ({ toolId, onSuccess }: RunToolFormProps) => {
         <Button variant="primary" type="submit" isLoading={isLoading}>
           Run Tool
         </Button>
-        <Button onClick={() => {reset(defaultValues); setSelectedOptions([])}} variant="outline">
+        <Button onClick={() => {reset(defaultValues); setFileStates({})}} variant="outline">
           Reset
         </Button>
       </ButtonGroup>
