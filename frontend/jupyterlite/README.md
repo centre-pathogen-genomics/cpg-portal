6977bab6a65c928baf4bee1fe37d7727f902ebbf
Wytamma Wirth <wytamma.wirth@me.com>
2025-03-05 00:03:52 +1100

    :art: sharedbuffer array for jupyterlite

--- frontend/jupyterlite/README.md
+++ frontend/jupyterlite/README.md
@@ -22,3 +22,15 @@ source .venv/bin/activate
 jupyter lite build
 ```
 
+# Hosting
+
+You need to set HEADERS to allow jupyterlite to load files. This is an example of how to do it in Apache (our reverse proxy):
+```
+<IfModule mod_headers.c>
+    <Location /wasm/jupyterlite>
+        Header always set Cross-Origin-Opener-Policy "same-origin"
+        Header always set Cross-Origin-Embedder-Policy "require-corp"
+        Header always set Cross-Origin-Resource-Policy "same-origin"
+    </Location>
+</IfModule>
+```
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,22 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    { // https://jupyterlite.readthedocs.io/en/stable/howto/content/python.html#synchronous-communication-with-the-kernel-over-atomics-wait-via-sharedarraybuffer
+      name: 'add-jupyter-headers', 
+      configureServer(server) {
+        // Middleware to add headers to every response
+        server.middlewares.use((req, res, next) => {
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          // Optionally include CORP
+          res.setHeader('Cross-Origin-Resource-Policy', 'same-origin');
+          next();
+        });
+      },
+    },
+
+  ],
 })
